035a246f1e61ff02771d1d741cb7659aa07ac7f5
Nicola Stroffolino <nicola.stroffolino@studio.unibo.it>
2025-11-12 11:24:55 +0100

    Ex. 82 added all runnable tests (defaults to fail) and empty DeathNoteImpl

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,42 @@
+package it.unibo.deathnote.impl;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    @Override
+    public String getRule(int ruleNumber) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    @Override
+    public void writeName(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,54 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+class TestDeathNote {
+    final DeathNote deathNote = new DeathNoteImpl();
+
+    private void isValidMessage(String msg) {
+        assertNotNull(msg); // Non-null message
+        assertFalse(msg.isBlank()); // Not a blank or empty message
+    }
+    
+    @Test
+    void testZeroOrNegativeRule() {
+        // try {
+        //     deathNote.getRule(0);
+        // } catch (final IllegalArgumentException e) {
+        //     isValidMessage(e.getMessage());
+        // }
+        
+        // try {
+        //     deathNote.getRule(-1);
+        // } catch (final IllegalArgumentException e) {
+        //     isValidMessage(e.getMessage());
+        // }
+        throw new AssertionError();
+    }
+
+    @Test
+    void testNullOrEmptyRules() {
+        throw new AssertionError();
+    }
+
+    @Test
+    void testHumanDeath() {
+        throw new AssertionError();
+    }
+
+    @Test
+    void testCauseOfDeath() {
+        throw new AssertionError();
+    }
+
+    @Test
+    void testDetailsOfDeath() {
+        throw new AssertionError();
+    }
 }
